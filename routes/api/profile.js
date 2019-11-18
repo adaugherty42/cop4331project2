9736333c95c38fb3599779bbd424c59a2488d05d
Adam <adaugherty42@gmail.com>
2019-11-18 18:02:47 -0500

    added methods to get all profiles and get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,4 +95,89 @@ router.post(
   }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Private
+router.get('/user/:user_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found.' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/profile/schedule
+// @desc    Add or update schedule
+// @access  Private
+router.put(
+  '/schedule',
+  [
+    auth,
+    [
+      check('monday', 'Monday is required')
+        .not()
+        .isEmpty(),
+      check('tuesday', 'Tuesday is required')
+        .not()
+        .isEmpty(),
+      check('wednesday', 'Wednesday is required')
+        .not()
+        .isEmpty(),
+      check('thursday', 'Thursday is required')
+        .not()
+        .isEmpty(),
+      check('friday', 'Friday is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { monday, tuesday, wednesday, thursday, friday } = req.body;
+
+    const schedule = {
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.schedule = schedule;
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
